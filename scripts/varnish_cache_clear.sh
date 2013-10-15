533a9292add9e7c4d356a3e100a299823cc4f173
tkuldeep <tkuldeep.singh@innoraft.com>
2013-10-15 19:03:34 +0530

    Add script for clearing cache on both webservers
     * How to clear cache using this script, please see comment in #45
     reference -> AamAadmiParty/india-standalone-drupal#45

---
+++ scripts/varnish_cache_clear.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clear complete cache in multiple web server 
+if [ -z $1]
+then
+  curl -X BAN http://54.254.102.251/
+  curl -X BAN http://54.251.248.135/
+
+# Clear cache of specific page 
+else
+  curl -X BAN http://54.254.102.251/$1
+  curl -X BAN http://54.251.102.251/$1
+fi
