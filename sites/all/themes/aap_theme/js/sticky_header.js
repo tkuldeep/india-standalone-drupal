6442607aa42f3462dd8c45277065de64e6ad797a
tkuldeep <tkuldeep.singh@innoraft.com>
2013-10-21 14:03:54 +0530

    Change js for making stick only countdown part
    reference -> AamAadmiParty/india-standalone-drupal#54

--- sites/all/themes/aap_theme/js/sticky_header.js
+++ sites/all/themes/aap_theme/js/sticky_header.js
@@ -1,18 +1,18 @@
 (function ($, Drupal, window, document, undefined) {
 	Drupal.behaviors.make_header_sticky = {
 			attach: function(context) { 
-				var header_menu = $('.main-menu-row');
+				var countdown = $('.aap-countdown-class');
 
 				 $(window).scroll(function(){
 					
 					var edge = $(window).scrollTop();
-					var os_header_menu = header_menu.offset().top;
+					var os_countdown = countdown.offset().top;
 
-					if (os_header_menu <= edge) {
-       						header_menu.addClass("sticky-header");
+					if (os_countdown <= edge) {
+       						countdown.addClass("sticky-header");
    					}
 					if (edge == 0) {
-						header_menu.removeClass("sticky-header");					
+						countdown.removeClass("sticky-header");					
 					}
 				});
 				
